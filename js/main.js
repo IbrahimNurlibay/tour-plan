5e7378e45e391d37eaa6e29c0647b411f40ab62a
IbrahimNurlibaeve <ibragim.nurlibay@mail.rum>
2020-07-25 23:39:24 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
   effect: "fade",
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-})
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+  effect: "fade",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
