266c481d1c1dd59fbbb79d31c31cd4c293ce6f49
IbrahimNurlibaeve <ibragim.nurlibay@mail.rum>
2020-08-03 18:22:10 +0300

    create: php скрипт для отправки email

--- send.php
+++ send.php
@@ -4,20 +4,6 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-// Формирование самого письма
-$title = "Новое обращение Best Tour Plan";
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
-";
-
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
@@ -30,7 +16,7 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'nurlibay.ibr.0002@gmail.com'; // Логин на почте
-    $mail->Password   = '750005poiqweIBR'; // Пароль на почте
+    $mail->Password   = ''; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('nurlibay.ibr.0002@gmail.com', 'Ибрахим Нурлибаев'); // Адрес самой почты и имя отправителя
@@ -38,10 +24,50 @@ try {
     // Получатель письма
     $mail->addAddress('nurlibev@mail.ru');  
 
+    // Переменные, которые отправляет пользователь
+    if (isset($_POST['submit_1'])) {
+    $email = $_POST['email'];
+
+    // отправка почты
+    $title_1 = "Новое обращение Best Tour Plan";
+    $body_1 = "
+    <h2>User email</h2>
+    <b>email:</b><br>$email
+    ";
+
+    // скрипт для отправки почты
+    $mail->isHTML(true);
+    $mail->Subject = $title_1;
+    $mail->Body = $body_1; 
+
+    // Отображение результата
+    header('Location: thankyou_two.html');
+    }
+
+
+    // Переменные, которые отправляет пользователь
+    if (isset($_POST['submit_2'])) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Формирование самого письма
+    $title_2 = "Новое обращение Best Tour Plan";
+    $body_2 = "
+    <h2>Новое письмо</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон:</b> $phone<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+
     // Отправка сообщения
     $mail->isHTML(true);
-    $mail->Subject = $title;
-    $mail->Body = $body;    
+    $mail->Subject = $title_2;
+    $mail->Body = $body_2; 
+
+    header('Location: thankyou.html');
+    }
+    
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
@@ -51,23 +77,3 @@ else {$result = "error";}
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
-
-// Переменные, которые отправляет пользователь
-if (isset($_POST['submit_1'])) {
-$email = $_POST['email'];
-
-// отправка почты
-$title_1 = "Новое обращение Best Tour Plan";
-$body_1 = "
-<h2>User email</h2>
-<b>email:</b><br>$email
-";
-
-    // скрипт для отправки почты
-    $mail->isHTML(true);
-    $mail->Subject = $title_1;
-    $mail->Body = $body_1; 
-}
-
-// Отображение результата
-header('Location: thankyou.html');
